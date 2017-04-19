988cdddbe55599ac273a54140c982b594946a0a5
DESKTOP-N0OL8UD\SONVH <sonvh1506@gmail.com>
2017-04-19 16:55:29 +0700

    A

--- SonvhShop.Model/Model/Footer.cs
+++ SonvhShop.Model/Model/Footer.cs
@@ -4,12 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace SonvhShop.Model.Model
 {
     [Table("Footers")]
-    internal class Footer
+    public class Footer
     {
         [Key]
         [MaxLength(50)]
         public string ID { get; set; }
-
         [Required]
         [MaxLength(500)]
         public string Content { get; set; }
